0eb1eae42c9d5b9bd9221be5a0a7861f8e94e6c9
analogy47 <actweed47@gmail.com>
2019-02-13 04:00:05 -0500

    add leading zero for time add greeting

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const date = new Date();
-const hours = document.querySelector('.hours');
-const minutes = document.querySelector('.minutes');
+const date = new Date()
+const hours = document.querySelector('.hours')
+const minutes = document.querySelector('.minutes')
 
-hours.textContent = date.getHours();
-minutes.textContent = date.getMinutes();
+hours.textContent = `0${date.getHours()}`.slice(-2)
+minutes.textContent = `0${date.getMinutes()}`.slice(-2)
